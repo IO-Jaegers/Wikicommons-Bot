9e172bef73771e58e3251bcb3ecbeabc6464aecf
kent vejrup madsen <kent.vejrup.madsen@outlook.com>
2022-11-08 17:10:50 +0100

    works as it is now

--- code/main.py
+++ code/main.py
@@ -1,12 +1,8 @@
-import pywikibot
-
+import frame
 
 def main():
-    Site = pywikibot.Site('commons:commons')
-    page = pywikibot.Page(Site, 'File:JerneSorgn-144908.jpg')
-    page.text = page.text.replace('|author=[[User:Designermadsen|Kent Vejrup Madsen]]', '|author={{Designermadsen:source}}')
-    page.save("test - Replacing author with template instead")
-
+    framework = frame.Frame()
+    framework.run()
 
 if __name__ == '__main__':
     main()
--- code/setup.sh
+++ code/setup.sh
@@ -4,5 +4,8 @@ python3 -m venv venv
 
 source ./venv/bin/activate
 
+pip install -U setuptools
+pip install requests
 pip install pywikibot
 pip install wikitextparser
+
